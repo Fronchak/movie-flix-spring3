89ff3b3798ef09221dfb3ec66079014df0b9e061
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-04-14 18:55:50 -0300

    Create entities movie and genre and configure connection to MySQL

---
+++ src/main/java/com/fronchak/movie_flix_spring3/entities/Genre.java
@@ -0,0 +1,66 @@
+package com.fronchak.movie_flix_spring3.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genre")
+public class Genre {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String name;
+	
+	@Column(columnDefinition = "TEXT", name = "image_url")
+	private String imageUrl;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ src/main/java/com/fronchak/movie_flix_spring3/entities/Movie.java
@@ -0,0 +1,114 @@
+package com.fronchak.movie_flix_spring3.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name =" movie")
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@Column(columnDefinition = "TEXT")
+	private String synopsis;
+	
+	@Column(name = "lauch_year")
+	private Integer lauchYear;
+	private Double rating;
+	
+	@Column(columnDefinition = "TEXT", name = "image_url")
+	private String imageUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "movie_genre",
+			joinColumns = @JoinColumn(name = "id_movie"),
+			inverseJoinColumns = @JoinColumn(name = "id_genre"))
+	private Set<Genre> genres = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	public Integer getLauchYear() {
+		return lauchYear;
+	}
+
+	public void setLauchYear(Integer lauchYear) {
+		this.lauchYear = lauchYear;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Set<Genre> getGenres() {
+		return genres;
+	}
+	
+	public void addGenre(Genre genre) {
+		genres.add(genre);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(id, other.id);
+	}
+}
