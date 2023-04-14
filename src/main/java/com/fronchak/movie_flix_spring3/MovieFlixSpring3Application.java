4343d45587b35eed99b87812845e95fdebf7250e
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-04-14 16:57:56 -0300

    Project initialized

---
+++ src/main/java/com/fronchak/movie_flix_spring3/MovieFlixSpring3Application.java
@@ -0,0 +1,13 @@
+package com.fronchak.movie_flix_spring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFlixSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFlixSpring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fronchak/movie_flix_spring3/MovieFlixSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronchak.movie_flix_spring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixSpring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
